d0e2e6008724bfe2149bf03cbf5a6dcc143dc0b7
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-06-17 19:38:18 +0500

    making analytics

--- app/analytics/page.js
+++ app/analytics/page.js
@@ -1,5 +1,17 @@
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 export default async function AnalyticsPage() {
   const session = await auth();
+
+  await connectMongo();
+  // get the user from the session.user.id, and only get the admin field.
+  const user = await User.findById(session.user.id, { isAdmin: 1 });
+
+  // if the user is not admin i.e the developer, then redirect to /
+  if (!user.isAdmin) redirect("/");
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen gap-3 text-center">
--- libs/mongoose.js
+++ libs/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (e) {
     console.error("❌ Mongoose Error: " + e.message);
   }
